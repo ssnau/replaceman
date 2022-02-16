7e4e0f569792fcec694a0d6422c9541729f6911c
korige <korige@gmail.com>
2022-02-16 19:07:17 +0800

    add autoLoop

--- readme.md
+++ readme.md
@@ -2,3 +2,14 @@ replaceman
 ------
 
 A simple replace tool for nodejs cli.
+
+
+### Install 
+
+```
+npm i replaceman -g
+```
+
+### How to use
+
+Run `replaceman` under any folder you want to manipulate on, it will tell you what to do.
--- replaceman.config.js
+++ replaceman.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  autoLoop: true, // Replaceman will automatically run the `replace` method in a loop until no further changes if autoLoop is true.
   /* filter files */
   filter: function(file) {
     return /md$/.test(file);
