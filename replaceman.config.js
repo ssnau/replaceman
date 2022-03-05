dbcb61d86d4d63107cfcec53f6bb60c3cd966a80
ssnau <korige@gmail.com>
2022-03-05 11:50:09 +0800

    standard fix

--- replaceman.config.js
+++ replaceman.config.js
@@ -3,28 +3,28 @@ module.exports = {
    * Replaceman will automatically run the `replace` method in a loop
    * until no further changes if autoLoop is true.
    */
-  autoLoop: true, 
-   /**
-    * Filter files. 
+  autoLoop: true,
+  /**
+    * Filter files.
     * Return `true` if don't want to do any filtering but it would be quite dangerous.
     * Better run with --dry before serious actions.
     * */
-  filter: function(file) {
-    return /md$/.test(file);
+  filter: function (file) {
+    return /md$/.test(file)
   },
-  /** 
-   * replace content with returned value 
+  /**
+   * replace content with returned value
    * autoLoop only make sense for this method.
    * */
-  replace: function(content) {
-    return content.replace('non-sense-string-for-content??!!)(*&@$%^%$#', "replaced");
+  replace: function (content) {
+    return content.replace('non-sense-string-for-content??!!)(*&@$%^%$#', 'replaced')
   },
   /**
    * rename files.
    * note: the paramters name the filename without path info.
    * */
-  rename: function(name) {
-    return name.replace('non-sense-string-for-filename', "no-way");
+  rename: function (name) {
+    return name.replace('non-sense-string-for-filename', 'no-way')
   },
   /**
    * exec command on files.
@@ -34,7 +34,7 @@ module.exports = {
    *   If you return empty string, null or undefined, nothing will be executed.
    *   Any string you return will be executed as command.
    * */
-   exec: function(absFilePath) {
-    return `ls ${absFilePath}`;
-  },
+  exec: function (absFilePath) {
+    return `ls ${absFilePath}`
+  }
 }
