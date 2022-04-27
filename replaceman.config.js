4b7037beb5a0deb6a5ae283eab9dee599beb5ad4
korige <korige@gmail.com>
2022-04-28 00:44:17 +0800

    rm autoloop

--- replaceman.config.js
+++ replaceman.config.js
@@ -1,18 +1,17 @@
 module.exports = {
-  autoLoop: true, // Replaceman will automatically run the `replace` method in a loop until no further changes if autoLoop is true.
   /* filter files */
   filter: function(file) {
     return /md$/.test(file);
   },
   /* replace content with returned value */
   replace: function(content) {
-    return content.replace('abcx', "ABCx");
+    return content.replaceAll('abcx', "ABCx");
   },
   /**
    * rename files.
    * note: the paramters name the filename without path info.
    * */
   rename: function(name) {
-    return name.replace('ffffffc', "cffffff");
+    return name.replace('<<file-name-wont-exists>>', "cffffff");
   },
 }
